fb3c40e1fc33e6603a6cce38c201ca8c88119370
Abdullah Alaslan <39031447+abdullahalaslan@users.noreply.github.com>
2020-11-17 14:32:43 +0300

    Update myApp.js

--- myApp/myApp.js
+++ myApp/myApp.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function(req, res) {
 res.writeHead(200);
-res.write('Hello World From NodeJS Abdullah');
+res.write('baris abi nolur gitme :/');
 res.end();
 });
 server.listen(8080);
