057edbe47632c88416d620bfee0b84d866cb9ec6
Abdullah Alaslan <39031447+abdullahalaslan@users.noreply.github.com>
2020-12-11 22:38:42 +0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function(req, res) {
 res.writeHead(200);
-res.write('Hello Hepsiburada from Abdullah');
+res.write('Hello from Abdullah');
 res.end();
 });
 server.listen(8080)
